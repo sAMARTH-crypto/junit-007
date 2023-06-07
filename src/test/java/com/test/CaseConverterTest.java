438a33af0ca98a84e2a29ba934f7c870dad9267b
sAMARTH-crypto <51755501+sAMARTH-crypto@users.noreply.github.com>
2023-06-06 23:10:22 -0400

    Update CaseConverterTest.java

--- src/test/java/com/test/CaseConverterTest.java
+++ src/test/java/com/test/CaseConverterTest.java
@@ -17,7 +17,7 @@ public class CaseConverterTest {
 	
 	@Test
 	public void testConvertCase7() {
-		assertEquals(c1.convertCase("ok"), "ok");
+		assertEquals(c1.convertCase("ok"), "OK");
 	}
 	
 	@Test
@@ -33,7 +33,7 @@ public class CaseConverterTest {
 	
 	@Test
 	public void testConvertCase6() {
-		assertEquals(c1.convertCase("7"), "7");
+		assertEquals(c1.convertCase("7"), "8");
 	}
 
 }
